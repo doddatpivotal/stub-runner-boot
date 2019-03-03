aea299930e2717fa85d49bc81a9973ef4d3fb55c
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-03-02 19:39:48 -0500

    Sets stub to use http port

--- src/main/java/io/pivotal/stubrunner/CfStubRunnerBootApplication.java
+++ src/main/java/io/pivotal/stubrunner/CfStubRunnerBootApplication.java
@@ -1,21 +1,47 @@
 package io.pivotal.stubrunner;
 
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import groovy.transform.CompileStatic;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.contract.stubrunner.HttpServerStubConfiguration;
+import org.springframework.cloud.contract.stubrunner.provider.wiremock.WireMockHttpServerStubConfigurer;
 import org.springframework.cloud.contract.stubrunner.server.EnableStubRunnerServer;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.util.SocketUtils;
 
 @SpringBootApplication
 @EnableStubRunnerServer
 @EnableDiscoveryClient
 public class CfStubRunnerBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CfStubRunnerBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CfStubRunnerBootApplication.class, args);
+    }
 
-	@AutoConfigureStubRunner
-	static class Config {}
+    @AutoConfigureStubRunner(httpServerStubConfigurer = HttpsForFortuneService.class)
+    static class Config {
+    }
+
+    @CompileStatic
+    static class HttpsForFortuneService extends WireMockHttpServerStubConfigurer {
+
+        private static final Log log = LogFactory.getLog(HttpsForFortuneService.class);
+
+        @Override
+        public WireMockConfiguration configure(WireMockConfiguration httpStubConfiguration, HttpServerStubConfiguration httpServerStubConfiguration) {
+            if (httpServerStubConfiguration.stubConfiguration.getArtifactId() == "fortune-service") {
+                int httpsPort = SocketUtils.findAvailableTcpPort();
+                log.info("Will set HTTPs port [" + httpsPort + "] for fraud detection server");
+                return httpStubConfiguration.httpsPort(httpsPort);
+            }
+            return httpStubConfiguration;
+        }
+
+    }
+
 }
 
